5b83e2baf72faef2c269476fe7e89581be6329af
griphin <64024490+griphin@users.noreply.github.com>
2024-07-20 10:49:45 +0800

    worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'b19aa4c6-1bd8-4b3d-a2d2-08160cb6bb6b';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
